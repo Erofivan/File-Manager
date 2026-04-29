a6ab6a506d36f03ca5bb6ab76cbcfdb78b76bde0
ivanerofeev <ivanerofeev01@gmail.com>
2026-04-29 11:22:58 +0300

    chore: remove extra folders

--- src/Lab1/Segments/IRouteSegment.cs
+++
@@ -1,9 +0,0 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab1.Entities;
-using Itmo.ObjectOrientedProgramming.Lab1.ResultTypes;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.Segments;
-
-public interface IRouteSegment
-{
-    SegmentTraversalResult Traverse(Train train);
-}
